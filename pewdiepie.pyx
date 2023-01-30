4061919b8cc8ce6620aab506994cc9ca7021c644
Tonmoy Islam <101395872+tonmoyislam250@users.noreply.github.com>
2023-01-30 13:29:50 +0600

    Update pewdiepie.pyx

--- pewdiepie.pyx
+++ pewdiepie.pyx
@@ -1,6 +1,7 @@
+import os
 from subprocess import run as srun
-UPSTREAM_REPO =$UPSTREAM_REPO
-UPSTREAM_BRANCH =$UPSTREAM_BRANCH 
+UPSTREAM_REPO =os.environ.get("UPSTREAM_REPO")
+UPSTREAM_BRANCH =os.environ.get("UPSTREAM_BRANCH")
 
 srun([f"git init -q \
 && git config --global user.email su6087031@gmail.com \
