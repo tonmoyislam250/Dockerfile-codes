40352584eb454ad18ccac69e7a9f3070a42e5ce2
Tonmoy Islam <101395872+tonmoyislam250@users.noreply.github.com>
2023-05-25 18:24:01 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-python3 race.py && python3 -m bot &&
+python3 race.py && pip3 install --no-cache-dir -r requirements3.txt && python3 -m bot &&
 cp ./extract /usr/local/bin &&
 cp ./pextract /usr/local/bin &&
 chmod +x /usr/local/bin/extract && chmod +x /usr/local/bin/pextract
